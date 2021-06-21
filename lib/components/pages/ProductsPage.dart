4a57dcc705fc317ceaa4e2c4dc790a53592cd48d
saucesar <sau.cesarlima2013@gmail.com>
2021-06-20 23:21:01 -0300

    Pagina de listagem de produtos

---
+++ lib/components/pages/ProductsPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../CustomDrawer.dart';
+
+class ProductsPage extends StatefulWidget {
+  const ProductsPage({ Key? key }) : super(key: key);
+
+  @override
+  _ProductsPageState createState() => _ProductsPageState();
+}
+
+class _ProductsPageState extends State<ProductsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Produtos'),
+        actions: [],
+      ),
+      drawer: CustomDrawer(context),
+    );
+  }
+}
